a06060cf2bbf0739b4a80539d9ff5ed4b42dd733
Machikhin Vadim <V.machikhin@bk.ru>
2022-02-02 22:44:39 +0500

    Firs commit

---
+++ src/Merchant.java
@@ -0,0 +1,14 @@
+public class Merchant implements Seller {
+    //Метод для продажи
+    public String sell(Goods goods) {
+        String result = "";
+        if (goods == Goods.POTION) {
+            result = "potion";
+        }
+        return result;
+    }
+    //Энам для товаров
+    public enum Goods {
+        POTION
+    }
+}
